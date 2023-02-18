6e1900bf557892154f695a9478ea9758157cc23a
Пользователь <Dzhevitskiy23@yandex.ru>
2023-02-18 15:38:21 +0700

    Объекты ДЗ

---
+++ skypro-objects/src/Author.java
@@ -0,0 +1,16 @@
+public class Author {
+
+    private String firstname;
+    private String lastname;
+
+    public Author(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    public String getFirstname() {
+        return firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+}
