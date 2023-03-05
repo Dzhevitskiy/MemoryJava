b3957aff24be2020894fb2db28b968f41ac51bdc
Dzhevitskiy <121280528+Dzhevitskiy@users.noreply.github.com>
2023-03-05 22:14:23 +0700

    Delete skypro-objects/src directory

--- skypro-objects/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        Author author = new Author("Роберт", "Грин");
-        Book book = new Book("33 стратегии войны", author, 2006);
-        System.out.println("Книга " + book.getTitle() + " Год " + book.getYear() + " Автор " + book.getAuthor().getFirstname() + " " + book.getAuthor().getLastname());
-    }
-}
